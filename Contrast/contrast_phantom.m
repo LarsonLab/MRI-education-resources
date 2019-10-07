a6918a8192a17efcc711eb00f81a6748e670b5a8
Peder Larson <peder.larson@ucsf.edu>
2019-10-06 23:12:48 -0700

    Checkpoint for new contrast material

--- Contrast/contrast_phantom.m
+++ Contrast/contrast_phantom.m
@@ -1,14 +1,26 @@
+function M = contrast_phantom(PDs, T1s, T2s, N, kspace_weighting);
 % Simulate a phantom setup with three circular regions, each with
 % different T1/T2 values
-addpath ../Utilities/
+
+% SPGR mode
+% sequence_params flip, TE, TR
+
+% FSE
+% kspace_TE, flip for partial refocusing?
+% kspace_flip, kspace_TE, kspace_TR
+
+% cover all simulations:
+% pass in simualation function and arguments, then run
+
 
 Nphan = 3;
 % phantom parameters
 xc = [-.3 0 .3]*256;  % x centers
 yc = [-.25 .25 -.25]*256;  % y centers
-T1 = [.5 .5 3];  % T1 relaxation times (s)
-T2 = [.02 .05 .1];  % T2 relaxation times (s)
+%T1 = [.5 .5 3];  % T1 relaxation times (s)
+%T2 = [.02 .05 .1];  % T2 relaxation times (s)
 r = 1/64;  % ball radius, where FOV = 1
+
 
 N = 256;
 
@@ -20,12 +32,11 @@ M = zeros(N,N);
 
 % Generate k-space data by adding together k-space data for each
 % individual phantom
-    % Note that this does not take into account the T1 and T2 relaxation
-    % rates for this phantom, T1(n) and T2(n)
 
 for n= 1:Nphan
     % Generates data using Fourier Transform of a circle (jinc) multiplied by complex exponential to shift center location
-    M = M+jinc(sqrt(kx.^2 + ky.^2) / (r) ) .* exp(i*2*pi* (kx*xc(n) + ky*yc(n)));
+    M = M+jinc(sqrt(kx.^2 + ky.^2) / (r) ) .* exp(i*2*pi* (kx*xc(n) + ky*yc(n))) * ...
+        MR_signal_spoiled_gradient_echo(flip, TE, TR, PDs(n), T1s(n), T2s(n));
 
 
 end
