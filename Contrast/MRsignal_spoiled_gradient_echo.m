0dc1d8f944e4f05c68be817bfb6c6519ffa76a69
Peder Larson <peder.larson@ucsf.edu>
2022-11-10 22:35:34 -0800

    Completed SNR notebook, image reconstruction part

--- Contrast/MRsignal_spoiled_gradient_echo.m
+++ Contrast/MRsignal_spoiled_gradient_echo.m
@@ -3,4 +3,4 @@ function S = MRsignal_spoiled_gradient_echo(flip, TE, TR, M0, T1, T2)
 %   Simulates the MRI signal in an spoiled gradient echo pulse sequence
 
 E1 = exp(-TR./T1);
-S = M0 .* sin(flip) .* exp(-TE/T2) .* (1-E1) ./ (1-E1*cos(flip)); 
+S = M0 .* sin(flip) .* exp(-TE/T2) .* (1-E1) ./ (1-E1.*cos(flip)); 
